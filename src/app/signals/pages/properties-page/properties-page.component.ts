c5d68a039e842f7643b1c04794af4022f7966ba2
santiagofer4 <santiagofernandezdelasierra@gmail.com>
2023-08-09 22:41:54 -0300

    section 329 finished

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,14 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './properties-page.component.html',
-  styleUrls: ['./properties-page.component.css']
+  styleUrls: ['./properties-page.component.css'],
 })
 export class PropertiesPageComponent {
 
+  public user = signal<User>({
+    id: 7,
+    email: 'michael.lawson@reqres.in',
+    first_name: 'Michael',
+    last_name: 'Lawson',
+    avatar: 'https://reqres.in/img/faces/7-image.jpg',
+  });
+
+  public fullName = computed( () => `${this.user().first_name} ${this.user().last_name}` );
+
+  onFieldUpdated(field: string, value: string) {
+
+    this.user.update( current => {
+      return {
+        ...current,
+        [field]: value,
+      }
+    })
 
 
-  onFieldUpdated( field: string, value: string ) {
-    console.log(field, value)
+    // this.user.mutate((current) => {
+    //   current.first_name = 'Hola Mundo';
+
+      // switch (field) {
+      //   case 'email':
+      //     current.email = value;
+      //     break;
+      //   case 'first_name':
+      //     current.first_name = value;
+      //     break;
+      //   case 'last_name':
+      //     current.last_name = value;
+      //     break;
+      //   case 'id':
+      //     current.id = Number(value);
+      //     break;
+      // }
+    // });
   }
 }
